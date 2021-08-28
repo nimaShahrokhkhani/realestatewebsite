1ad87b9b687db3d1448c88cf2067e6da70f81106
nima shahrokhkhani <nima.shahrokhkhani@gmail.com>
2021-08-29 00:12:56 +0430

    submit advertise

--- src/realState/Submit/submit.js
+++ src/realState/Submit/submit.js
@@ -104,7 +104,6 @@ class submit extends React.Component {
                         </div>
                     </div>
 
-
                     <div className="container">
                         <div className="row">
 
@@ -117,14 +116,14 @@ class submit extends React.Component {
 
                                         <div className="form">
                                             <h5>عنوان ملک </h5>
-                                            <input className="search-field" type="text"/>
+                                            <input className="search-field" type="text" id="titleSubmit"/>
                                         </div>
 
                                         <div className="row with-forms">
 
                                             <div className="col-md-6">
                                                 <h5>وضعیت</h5>
-                                                <select className="chosen-select-no-single">
+                                                <select className="chosen-select-no-single" id="conditionSubmit">
                                                     <option label="blank"></option>
                                                     <option>فروشی</option>
                                                     <option>اجاره ای</option>
@@ -133,12 +132,17 @@ class submit extends React.Component {
 
                                             <div className="col-md-6">
                                                 <h5>نوع</h5>
-                                                <select className="chosen-select-no-single">
+                                                <select className="chosen-select-no-single" id="kindSubmit">
                                                     <option label="blank"></option>
-                                                    <option>آپارتمان</option>
+                                                    <option>همه</option>
+                                                    <option>آپارتمانی</option>
                                                     <option>خانه</option>
                                                     <option>تجاری</option>
-                                                    <option>گاراژ</option>
+                                                    <option>ویلا</option>
+                                                    <option>زمین کشاورزی</option>
+                                                    <option>اداری</option>
+                                                    <option>زمین</option>
+                                                    <option>مستغلات</option>
                                                 </select>
                                             </div>
 
@@ -148,7 +152,7 @@ class submit extends React.Component {
 
                                             <div className="col-md-4">
                                                 <h5>قیمت </h5>
-                                                <div className="select-input disabled-first-option">
+                                                <div className="select-input disabled-first-option" id="priceSubmit">
                                                     <input type="text" data-unit="تومان"/>
                                                 </div>
                                             </div>
@@ -156,13 +160,13 @@ class submit extends React.Component {
                                             <div className="col-md-4">
                                                 <h5>مساحت</h5>
                                                 <div className="select-input disabled-first-option">
-                                                    <input type="text" data-unit="متر مربع"/>
+                                                    <input type="text" data-unit="متر مربع" id="areaSubmit"/>
                                                 </div>
                                             </div>
 
                                             <div className="col-md-4">
                                                 <h5>اتاق</h5>
-                                                <select className="chosen-select-no-single">
+                                                <select className="chosen-select-no-single" id="roomSubmit">
                                                     <option label="blank"></option>
                                                     <option>1</option>
                                                     <option>2</option>
@@ -179,7 +183,7 @@ class submit extends React.Component {
 
                                     <h3>گالری</h3>
                                     <div className="submit-section">
-                                        <form action="/file-upload" className="dropzone"></form>
+                                        <form action="/file-upload" className="dropzone" id="gallerySubmit"></form>
                                     </div>
 
 
@@ -190,22 +194,22 @@ class submit extends React.Component {
 
                                             <div className="col-md-6">
                                                 <h5>آدرس</h5>
-                                                <input type="text"/>
+                                                <input type="text" id="addressSubmit"/>
                                             </div>
 
                                             <div className="col-md-6">
                                                 <h5>شهر</h5>
-                                                <input type="text"/>
+                                                <input type="text" id="citySubmit"/>
                                             </div>
 
                                             <div className="col-md-6">
                                                 <h5>استان</h5>
-                                                <input type="text"/>
+                                                <input type="text" id="provinceSubmit"/>
                                             </div>
 
                                             <div className="col-md-6">
                                                 <h5>کد پستی</h5>
-                                                <input style={{direction:'ltr'}} type="text"/>
+                                                <input style={{direction: "ltr"}} type="text" id="codePostAddress"/>
                                             </div>
 
                                         </div>
@@ -219,14 +223,14 @@ class submit extends React.Component {
                                         <div className="form">
                                             <h5>توضیحات</h5>
                                             <textarea className="WYSIWYG" name="summary" cols="40" rows="3" id="summary"
-                                                      spellCheck="true"></textarea>
+                                                      spellCheck="true" id="explainSubmit"/>
                                         </div>
 
                                         <div className="row with-forms">
 
                                             <div className="col-md-4">
                                                 <h5>عمر ساخت <span>(اختیاری)</span></h5>
-                                                <select className="chosen-select-no-single">
+                                                <select className="chosen-select-no-single" id="ageSubmit">
                                                     <option label="blank"></option>
                                                     <option>0 - 1 سال</option>
                                                     <option>0 - 5 سال</option>
@@ -237,21 +241,10 @@ class submit extends React.Component {
                                                 </select>
                                             </div>
 
-                                            <div className="col-md-4">
-                                                <h5>اتاق خواب <span>(اختیاری)</span></h5>
-                                                <select className="chosen-select-no-single">
-                                                    <option label="blank"></option>
-                                                    <option>1</option>
-                                                    <option>2</option>
-                                                    <option>3</option>
-                                                    <option>4</option>
-                                                    <option>5</option>
-                                                </select>
-                                            </div>
 
                                             <div className="col-md-4">
                                                 <h5>حمام <span>(اختیاری)</span></h5>
-                                                <select className="chosen-select-no-single">
+                                                <select className="chosen-select-no-single" id="bathSubmit">
                                                     <option label="blank"></option>
                                                     <option>1</option>
                                                     <option>2</option>
@@ -260,75 +253,92 @@ class submit extends React.Component {
                                                     <option>5</option>
                                                 </select>
                                             </div>
+                                            <div className="col-md-4">
+                                                <h5>در اختیار مشاورین املاک <span></span></h5>
+                                                <div className="allow-house">
+                                                    <input type="radio" checked="checked" value="male" name="gender"
+                                                           id="yesSubmit"/> قرار بگیرد<br/>
+                                                    <input type="radio" value="female" name="gender"
+                                                           id="noSubmit"/> قرار نگیرد
+
+
+                                                </div>
+
+                                            </div>
+
+
+                                            <h5 className="margin-top-30">ویژگی های دیگر <span>(اختیاری)</span></h5>
+                                            <div className="checkboxes in-row margin-bottom-20">
+
+                                                <input id="airConditionSubmit" type="checkbox" name="check"/>
+                                                <label htmlFor="airConditionSubmit">تهویه مطبوع</label>
+
+                                                <input id="poolSubmit" type="checkbox" name="check"/>
+                                                <label htmlFor="poolSubmit">استخر</label>
+
+                                                <input id="centerHeaterSubmit" type="checkbox" name="check"/>
+                                                <label htmlFor="centerHeaterSubmit">گرمایش مرکزی</label>
+
+                                                <input id="roomWashingSubmit" type="checkbox" name="check"/>
+                                                <label htmlFor="roomWashingSubmit">اتاق لباسشویی</label>
+
+
+                                                <input id="gymClubSubmit" type="checkbox" name="check"/>
+                                                <label htmlFor="gymClubSubmit">باشگاه
+                                                    بدنسازی</label>
+
+                                                <input id="dangerRingSubmit" type="checkbox"
+                                                       name="check"/>
+                                                <label htmlFor="dangerRingSubmit">زنگ
+                                                    خطر</label>
+
+                                                <input id="windowSubmit" type="checkbox"
+                                                       name="check"/>
+                                                <label htmlFor="windowSubmit">پوشش
+                                                    پنجره</label>
+
+                                            </div>
 
                                         </div>
 
 
-                                        <h5 className="margin-top-30">ویژگی های دیگر <span>(اختیاری)</span></h5>
-                                        <div className="checkboxes in-row margin-bottom-20">
+                                        <h3>اطلاعات تماس</h3>
+                                        <div className="submit-section">
 
-                                            <input id="check-2" type="checkbox" name="check"/>
-                                            <label htmlFor="check-2">تهویه مطبوع</label>
+                                            <div className="row with-forms">
 
-                                            <input id="check-3" type="checkbox" name="check"/>
-                                            <label htmlFor="check-3">استخر</label>
+                                                <div className="col-md-4">
+                                                    <h5>نام</h5>
+                                                    <input type="text" id="nameSubmit"/>
+                                                </div>
 
-                                            <input id="check-4" type="checkbox" name="check"/>
-                                            <label htmlFor="check-4">گرمایش مرکزی</label>
+                                                <div className="col-md-4">
+                                                    <h5>ایمیل</h5>
+                                                    <input type="text" style={{direction: "ltr"}} id="emailSubmit"/>
+                                                </div>
 
-                                            <input id="check-5" type="checkbox" name="check"/>
-                                            <label htmlFor="check-5">اتاق لباسشویی</label>
+                                                <div className="col-md-4">
+                                                    <h5>تلفن <span>(اختیاری)</span></h5>
+                                                    <input type="text" style={{direction: "ltr"}} id="telSubmit"/>
+                                                </div>
 
-
-                                            <input id="check-6" type="checkbox" name="check"/>
-                                            <label htmlFor="check-6">باشگاه بدنسازی</label>
-
-                                            <input id="check-7" type="checkbox" name="check"/>
-                                            <label htmlFor="check-7">زنگ خطر</label>
-
-                                            <input id="check-8" type="checkbox" name="check"/>
-                                            <label htmlFor="check-8">پوشش پنجره</label>
+                                            </div>
 
                                         </div>
+
+
+                                        <div className="divider"></div>
+                                        <a className="button preview margin-top-5">ثبت <i
+                                            className="fa fa-arrow-circle-left"></i></a>
 
                                     </div>
-
-
-                                    <h3>اطلاعات تماس</h3>
-                                    <div className="submit-section">
-
-                                        <div className="row with-forms">
-
-                                            <div className="col-md-4">
-                                                <h5>نام</h5>
-                                                <input type="text"/>
-                                            </div>
-
-                                            <div className="col-md-4">
-                                                <h5>ایمیل</h5>
-                                                <input type="text" style={{direction:'ltr'}}/>
-                                            </div>
-
-                                            <div className="col-md-4">
-                                                <h5>تلفن <span>(اختیاری)</span></h5>
-                                                <input type="text" style={{direction:'ltr'}}/>
-                                            </div>
-
-                                        </div>
-
-                                    </div>
-
-
-                                    <div className="divider"></div>
-                                    <a href="#" className="button preview margin-top-5">ثبت <i
-                                        className="fa fa-arrow-circle-left"></i></a>
-
                                 </div>
+
                             </div>
 
                         </div>
-                    </div>
 
+                    </div>
                 </div>
             </div>
         );
