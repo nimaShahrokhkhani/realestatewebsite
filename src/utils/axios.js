c44a3e1e92b2984f8da14695191f9f801ce9491f
nima shahrokhkhani <nima.shahrokhkhani@gmail.com>
2021-12-31 11:35:27 +0330

    revision

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 // let baseUrl = 'http://gilanfile.ir:3600/';
 // let baseUrl = 'http://localhost:3600/';
-let baseUrl = 'https://api.gilanfile.ir/';
+let baseUrl = 'https://gilanfile.ir/';
 
 const instance = axios.create({
     baseURL: baseUrl
